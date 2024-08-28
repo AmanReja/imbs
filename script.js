846ce60dc5dc6ebfbdbebfbad42f1df8fc6d2d76
Aman Reja <amnmolla742@gmail.com>
2024-08-28 10:17:07 +0530

    fixed lag

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ let banner = document.querySelector(".aman");
 console.log(banner);
 let heading = document.querySelector(".hedline-container");
 let headingh = heading.getElementsByTagName("h1");
+let social = document.querySelector("#social-container");
+let email = document.querySelector("#emailing-container");
 
 ScrollReveal().reveal(banner, { delay: 500, origin: "left" });
-ScrollReveal().reveal(headingh, { delay: 500, origin: "up" });
+ScrollReveal().reveal(headingh, { delay: 500, origin: "top" });
+ScrollReveal().reveal(social, { delay: 500, origin: "top" });
+ScrollReveal().reveal(email, {
+  delay: 400,
+  origin: "right",
+  easing: "ease-in",
+});
